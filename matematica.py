b646e6fad799f2b8914b319c437503f194a76957
LuanPicolo <161030989+LuanPicolo@users.noreply.github.com>
2024-08-15 09:45:18 -0300

    Add files via upload

---
+++ matematica.py
@@ -0,0 +1,31 @@
+#print("Olá Mudo!")
+
+#n1 = input("numero 1: ")
+#n2 = input("numero 2: ")
+#n3 = input("numero 3: ")
+#resultado = int(n1) + int(n2) + int(n3)
+#print("The result of some dos numero: ", resultado)
+
+#fruta = input ("Digite o nome da tua fruta favorita filho da puta ^^: ")
+#print ("A fruta do arrombado é:", fruta)
+
+#distanciaKM = input("Coloque a distancia da cidade...:")
+#distanciaM = float(distanciaKM) * 1000
+#print("A distancia em metros é: ", distanciaM, "B)")
+
+#nota1 = input("Digite a primeira nota: ")
+#nota2 = input("Digite a segunda nota: ")
+#mediaSemestre = (float(nota1) + float(nota2)) / 2
+#print("Sua média é:", mediaSemestre)
+
+#ladoh = input ("Qual o tamanho do lado do seu quadrado?: ")
+#areaQuadrado = float(ladoh) ** 2
+#print ("A área do quadrado é", areaQuadrado)
+
+#raioCirculo = input("Qual o raio do circulo?: ")
+#areaCirculo = (float(raioCirculo) ** 2) * 3.1415
+#print("Toma sua area ai patrão: ", areaCirculo)
+
+grausFarenheit = input("Digite a temperatura em Farenheit: ")
+grausCelsius = (float(grausFarenheit) - 32) / 1.8
+print("A temperatura em Celsius é: ", round(grausCelsius))
